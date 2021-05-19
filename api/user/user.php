57493aab32ec837268238ace89037bfbe1d0f9aa
Julie ARCHER <julie.archer@viacesi.fr>
2021-05-19 15:25:54 +0200

    Added insertUser function to User

--- api/user/user.php
+++ api/user/user.php
@@ -31,5 +31,19 @@
 			}
 			return (true);
 		}
+		
+		public function	insertUser($conn) {
+			$encodedpass = hash('sha256', $this->password);
+            $insert_query = "INSERT INTO `user`(name,email,password, firstname,role) VALUES(:name,:email,:password,:firstname,:role)";
+
+            $insert_stmt = $conn->prepare($insert_query);
+            // DATA BINDING
+            $insert_stmt->bindValue(':name', htmlspecialchars(strip_tags($this->name)), PDO::PARAM_STR);
+            $insert_stmt->bindValue(':firstname', htmlspecialchars(strip_tags($this->firstname)), PDO::PARAM_STR);
+            $insert_stmt->bindValue(':email', htmlspecialchars(strip_tags($this->email)), PDO::PARAM_STR);
+            $insert_stmt->bindValue(':password', htmlspecialchars(strip_tags($encodedpass)), PDO::PARAM_STR);
+            $insert_stmt->bindValue(':role', htmlspecialchars(strip_tags($this->role)), PDO::PARAM_STR);
+			return $insert_stmt->execute();
+		}
 	}
 ?>
