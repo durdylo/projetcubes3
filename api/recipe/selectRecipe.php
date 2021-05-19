27c3153039b8e070d6c0026b86b54029dc3d22a8
user.nale <loic.albert.denis@gmail.com>
2021-05-19 15:30:21 +0200

    discard commit

--- api/recipe/selectRecipe.php
+++ api/recipe/selectRecipe.php
@@ -12,7 +12,7 @@ $db_connection = new Database();
 $conn = $db_connection->dbConnection();
 
 // GET DATA FORM REQUEST
-$data = json_decode(file_get_contents("php://input"));
+$data = json_decode(file_get_contents("php://input"), true);
 
 //CREATE MESSAGE ARRAY AND SET EMPTY
 $msg['message'] = '';
