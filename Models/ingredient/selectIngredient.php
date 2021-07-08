369f2b3f85f24754d76662642a98cd1c828238c7
Julie ARCHER <julie.archer@viacesi.fr>
2021-07-08 14:55:10 +0200

    require database to require_once database

--- Models/ingredient/selectIngredient.php
+++ Models/ingredient/selectIngredient.php
@@ -10,7 +10,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 // INCLUDING DATABASE AND MAKING OBJECT
-require '../../database.php';
+require_once('../../database.php');
 $db_connection = new Database();
 $conn = $db_connection->dbConnection();
 
